f73948df035f2c80b16a9036ed0895ed102f6637
shenwilly <shen.willy95@gmail.com>
2023-09-28 23:59:21 +0700

    limit models

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -20,9 +20,10 @@ Respond using Markdown.`;
 
 export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
-  'gpt-3.5-turbo-16k',
+  // 'gpt-3.5-turbo-16k',
   'gpt-4',
-  'gpt-4-32k',
+  // 'gpt-4-32k',
+  
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
   // 'gpt-4-32k-0314',
